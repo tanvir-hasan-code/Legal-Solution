8e3dca3898e2537bf423159f6b303503d56271ae
tanvir-hadan-code <tanvirx015@gmail.com>
2025-10-27 15:27:09 +0600

    Project Section Add

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+<div class="card bg-base-100 image-full shadow-sm">
+              <figure>
+                <img class="w-full"
+                  src="images/slider-1.jpg"
+                  alt="Client-Say" />
+              </figure>
+              <div class="card-body my-auto gap-8">
+                <div class="avatar avatar-online">
+                  <div class="w-14 mx-auto md:w-24 rounded-full ring ring-ofset-2 ring-primary">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  </div>
+                </div>
+                <h1 class="text-xl md:text-4xl text-center  md:font-bold font-semibold">Tanvir Hasan</h1>
+                <p class="text-center text-xs mt-6 md:text-xl max-w-4xl mx-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure tempore facilis eveniet quam magnam a unde molestias hic odit assumenda veritatis exercitationem error, fugit neque inventore numquam minus quidem autem est! Possimus, expedita libero.</p>
+              </div>
+            </div>
